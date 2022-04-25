6ef527628d6086b717aa3e316903b9ef3c3dee54
Umesh Panwar <umeshpanwar@microsoft.com>
2022-04-25 13:05:55 -0400

    removed network block from the example

--- samples/201-cosmosdb-firewall/main.tf
+++ samples/201-cosmosdb-firewall/main.tf
@@ -17,35 +17,6 @@ resource "azurerm_resource_group" "this" {
   location = var.location
 }
 
-# Virtual Network for private endpoint 
-resource "azurerm_virtual_network" "this" {
-  name                = var.virtual_network_name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  address_space       = var.vnet_address_space
-}
-
-resource "azurerm_subnet" "this" {
-  name                                           = var.subnet_name
-  resource_group_name                            = azurerm_resource_group.this.name
-  virtual_network_name                           = azurerm_virtual_network.this.name
-  address_prefixes                               = var.subnet_prefixes
-  enforce_private_link_endpoint_network_policies = true
-}
-
-# Private DNS Zone for SQL API 
-resource "azurerm_private_dns_zone" "this" {
-  name                = "privatelink.documents.azure.com"
-  resource_group_name = azurerm_resource_group.this.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "this" {
-  name                  = var.private_dns_vnet_link_name
-  resource_group_name   = azurerm_resource_group.this.name
-  private_dns_zone_name = azurerm_private_dns_zone.this.name
-  virtual_network_id    = azurerm_virtual_network.this.id
-  registration_enabled  = false
-}
 
 module "azure_cosmos_db" {
   source              = "../../modules/cosmos_db"
@@ -58,23 +29,7 @@ module "azure_cosmos_db" {
   public_network_access_enabled = var.public_network_access_enabled
   ip_firewall_enabled           = var.ip_firewall_enabled
   firewall_ip                   = var.firewall_ip
-  private_endpoint = {
-    "pe_endpoint" = {
-      dns_zone_group_name             = var.dns_zone_group_name
-      dns_zone_rg_name                = azurerm_private_dns_zone.this.resource_group_name
-      is_manual_connection            = false
-      name                            = var.pe_name
-      private_service_connection_name = var.pe_connection_name
-      subnet_name                     = azurerm_subnet.this.name
-      vnet_name                       = azurerm_virtual_network.this.name
-      vnet_rg_name                    = azurerm_resource_group.this.name
-    }
-  }
   depends_on = [
-    azurerm_resource_group.this,
-    azurerm_virtual_network.this,
-    azurerm_subnet.this,
-    azurerm_private_dns_zone.this,
-    azurerm_private_dns_zone_virtual_network_link.this
+    azurerm_resource_group.this
   ]
 }
--- samples/201-cosmosdb-firewall/variables.tf
+++ samples/201-cosmosdb-firewall/variables.tf
@@ -8,54 +8,6 @@ variable "location" {
   description = "Azure Region"
 }
 
-variable "virtual_network_name" {
-  type        = string
-  description = "Virtual Network Name"
-  default     = "samplevnet_202"
-}
-
-variable "vnet_address_space" {
-  type        = list(string)
-  description = "VNET Address Prefixes"
-  default     = ["10.0.0.0/16"]
-}
-
-variable "subnet_name" {
-  type        = string
-  description = "VNET Subnet Name"
-  default     = "pe_subnet"
-}
-
-variable "subnet_prefixes" {
-  type        = list(string)
-  description = "Subnet Address Prefixes"
-  default     = ["10.0.0.0/24"]
-}
-
-variable "private_dns_vnet_link_name" {
-  type        = string
-  description = "Private DNS Zone Link Name"
-  default     = "sqlapi_zone_link"
-}
-
-variable "dns_zone_group_name" {
-  type        = string
-  description = "Zone Group Name for PE"
-  default     = "pe_zone_group"
-}
-
-variable "pe_name" {
-  type        = string
-  description = "Private Endpoint Name"
-  default     = "cosmosdb_pe"
-}
-
-variable "pe_connection_name" {
-  type        = string
-  description = "Private Endpoint Connection Name"
-  default     = "pe_connection"
-}
-
 variable "public_network_access_enabled" {
   type        = bool
   description = "Enable public access"
