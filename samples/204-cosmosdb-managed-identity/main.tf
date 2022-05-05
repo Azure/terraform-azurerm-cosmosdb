6ed2e2661d8f0428c28e14b4634a7bb6d0ce0a1b
Gin <ginc11232@gmail.com>
2022-05-05 15:38:11 +0000

    Removed user assigned identity capability with azapi as there appears to be lifecycle issues which may be unstable (ex. can't switch from userassigned->systemassigned)

--- samples/204-cosmosdb-managed-identity/main.tf
+++ samples/204-cosmosdb-managed-identity/main.tf
@@ -18,18 +18,6 @@ resource "azurerm_resource_group" "this" {
   location = var.location
 }
 
-resource "azurerm_user_assigned_identity" "this" {
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  name                = "cosmosdb_uai"
-}
-
-/* 
-Notes for different managed identity settings
- - Set identity.type = "SystemAssigned" and id ="" for system-assigned only
- - Set identity.type = "UserAssigned" and id = azurerm_user_assigned_identity.this.id for user-assigned only 
- - Set identity.type = "SystemAssigned,UserAssigned" and id = azurerm_user_assigned_identity.this.id  for both user and system assigned identity
-*/
 module "azure_cosmos_db" {
   source              = "../../modules/cosmos_db"
   resource_group_name = azurerm_resource_group.this.name
@@ -38,13 +26,8 @@ module "azure_cosmos_db" {
   cosmos_api          = var.cosmos_api
   sql_dbs             = var.sql_dbs
   sql_db_containers   = var.sql_db_containers
-  identity = {
-    enabled = true
-    type    = "UserAssigned"
-    id      = azurerm_user_assigned_identity.this.id
-  }
+  enable_systemassigned_identity = true 
   depends_on = [
     azurerm_resource_group.this,
-    azurerm_user_assigned_identity.this
   ]
 }
